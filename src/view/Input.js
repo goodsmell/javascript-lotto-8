426d4abdb5e5c2101f09368a449df48ffefeaba9
goodsmell <87801306+goodsmell@users.noreply.github.com>
2025-11-02 23:13:15 +0900

    feat(input): 보너스 번호 입력 기능 구현

--- src/view/Input.js
+++ src/view/Input.js
@@ -2,12 +2,16 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 
 export class Input {
   static async askWinningNumber() {
-    const input = await MissionUtils.Console.readLineAsync('당첨 번호를 입력해주세요.\n');
-    return input; 
+    const input = await MissionUtils.Console.readLineAsync('\n당첨 번호를 입력해주세요.\n');
+    return input;
   }
 
   static async askPurchaseAmount() {
     const input = await MissionUtils.Console.readLineAsync('구입금액을 입력해주세요.\n');
-    return input; 
+    return input;
+  }
+  static async askBonusNumber() {
+    const input = await MissionUtils.Console.readLineAsync('\n보너스 번호를 입력해 주세요.\n');
+    return input;
   }
 }
